b48b624a9d1e86a086f2d1a3516f35a713639f02
olin-med <olin.costa@sou.inteli.edu.br>
2024-06-25 17:22:24 -0300

    fix mobbile

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,14 +1,15 @@
+import React from 'react';
 import { useNavigate } from 'react-router-dom';
 
 export function Footer() {
   const navigate = useNavigate();
 
   return (
-    <footer className="flex flex-col md:flex-row justify-between items-center gap-5 self-stretch px-5 md:px-20 py-4 mt-10 md:mt-44 w-full text-3xl bg-liberty-cyan">
+    <footer className="flex flex-col md:flex-row justify-between items-center gap-5 self-stretch px-5 md:px-20 py-8 md:py-4 mt-10 md:mt-44 w-full text-3xl bg-liberty-cyan">
       <img
         loading="lazy"
         src="/logo2.png"
-        className="shrink-0 max-w-full aspect-[0.85] scale-90 w-[133px] cursor-pointer mb-4 md:mb-0 -me-44"
+        className="hidden md:block shrink-0 max-w-full aspect-[0.85] scale-90 w-[133px] cursor-pointer mb-4 md:mb-0 -me-40"
         onClick={() => navigate('/')}
       />
       <div 
